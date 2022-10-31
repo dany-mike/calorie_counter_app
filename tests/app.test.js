b0b6c46b66b8557ab93aaf368037327e4d9509f2
Dany <danymike.joseph@gmail.com>
2022-10-31 16:14:16 +0100

    feat: Hello World! endpoint TDD refacto

--- tests/app.test.js
+++ tests/app.test.js
@@ -3,23 +3,6 @@ const request = require("supertest");
 const app = require("../src/app");
 
 describe("Test the root path", () => {
-  const port = 3000;
-  let appServer = app.listen(port, () =>
-    console.log(`Listening on port ${port}`)
-  );
-
-  beforeEach(function () {
-    delete require.cache[require.resolve("../src/app")];
-  });
-
-  afterEach(function (done) {
-    appServer.close(done);
-  });
-
-  afterAll(async () => {
-    await new Promise((resolve) => setTimeout(() => resolve(), 500)); // avoid jest open handle error
-  });
-
   test("It says Hello World!", (done) => {
     request(app)
       .get("/")
